a547d991a563d9ee3d583ba39300b69a17e29be7
Shubham Sahu <shubham.sahu.techie@gmail.com>
2026-03-25 19:19:10 +0530

    second commit book and course

---
+++ src/Book.java
@@ -0,0 +1,63 @@
+public class Book {
+
+    static int toNoOfBook;
+
+    String title;
+
+    String author;
+
+    String isbn;
+
+    boolean isBorrowed;
+
+    static {
+        toNoOfBook = 0;
+    }
+
+    {
+        toNoOfBook++;
+    }
+
+    Book(String isbn, String title, String author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+    }
+
+    Book(String isbn) {
+        this(isbn, "UnKNOW", "Unknown");
+    }
+
+    static int getToNoOfBook() {
+        return toNoOfBook;
+    }
+
+    void borrowBook() {
+        if (isBorrowed) {
+            System.out.println("book is alreeady borrowed");
+        } else {
+            this.isBorrowed = true;
+            System.out.println("enjoy the book" + this.title);
+        }
+    }
+
+    void returnBook() {
+        if (isBorrowed) {
+            this.isBorrowed = false;
+            System.out.println("hope enjoy the book leave feedback");
+        } else {
+            System.out.println("this book already in the libraary");
+        }
+    }
+
+    public static void main(String[] args) {
+        Book yogi = new Book("1", "biography", "yogi guru");
+        Book myBook = new Book("2");
+        yogi.borrowBook();
+        myBook.borrowBook();
+        yogi.borrowBook();
+        yogi.returnBook();
+        yogi.returnBook();
+    }
+
+}
---
+++ src/Course.java
@@ -0,0 +1,35 @@
+public class Course {
+
+    static int maxCapacity;
+
+    String courseName;
+
+
+    int enrollments;
+
+    String[] enrolledStudents;
+
+
+    Course(String courseName) {
+        this.courseName = courseName;
+        this.enrollments = 0;
+        this.enrolledStudents = new String[maxCapacity];
+    }
+
+
+    static void setMaxCapacity(int maxCapacity) {
+        Course.maxCapacity = maxCapacity;
+    }
+
+    void enrollStudent(String studentName) {
+        enrolledStudents[enrollments] = studentName;
+        enrollments++;
+
+    }
+
+    void unEnrollStudent(String studentName) {
+        System.out.println("student remove");
+        enrollments--;
+    }
+
+}
