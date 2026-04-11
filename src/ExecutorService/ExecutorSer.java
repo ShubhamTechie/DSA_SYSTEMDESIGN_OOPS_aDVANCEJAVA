2fa0da45d91e595d106bc16203ebe009622a8f37
Shubham Sahu <shubham.sahu.techie@gmail.com>
2026-04-11 15:39:34 +0530

    executor serviceG

--- src/ExecutorService/ExecutorSer.java
+++ src/ExecutorService/ExecutorSer.java
@@ -6,7 +6,8 @@ public class ExecutorSer {
     public static void main(String[] args) {
 
 
-        System.out.println("executir service"
-        );
+        System.out.println("executir service");
+
+        System.out.println("its a ,multithreading concept to use multiple thread witout manually  things top do");
     }
 }
