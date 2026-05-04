169c8589857d084dcaaa2c04a2aaafa2ab817802
Shubham Sahu <shubham.sahu.techie@gmail.com>
2026-05-04 21:02:03 +0530

    xhjhasjdam

--- src/Devops/LoggingTools.java
+++ src/Devops/LoggingTools.java
@@ -5,5 +5,20 @@ public class LoggingTools {
     public static void main(String[] args) {
 
         System.out.println(";pgiing system like elk logging kibana elastic serch ");
+        System.out.println(";pgiing system like elk logging kibana elastic serch ");
+
+        System.out.println(";pgiing system like elk logging kibana elastic serch ");
+
+        System.out.println(";pgiing system like elk logging kibana elastic serch ");
+
+        System.out.println(";pgiing system like elk logging kibana elastic serch ");
+
+        System.out.println(";pgiing system like elk logging kibana elastic serch ");
+
+        System.out.println(";pgiing system like elk logging kibana elastic serch ");
+
+        System.out.println(";pgiing system like elk logging kibana elastic serch ");
+
+
     }
 }
